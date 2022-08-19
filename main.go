bbeec8523c7d0d3f6878ed313b4a83b32c5890f1
FrancescoLuzzi <francescoluzzi99@libero.it>
2022-08-20 00:04:49 +0200

    BIG refactor, operations collapsed onto a single function

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strings"
 	"sync"
 )
 
 const NO_BASE_CMD_ERROR = 1
 const WRONG_MULTI_CMD_ERROR = 2
+
+type operator func(x float64, y float64) (float64, error)
 
 func add(x float64, y float64) (float64, error) {
 	return x + y, nil
@@ -28,7 +31,7 @@ func multiply(x float64, y float64) (float64, error) {
 	return x * y, nil
 }
 
-var FUNCTION_MAP = map[byte]func(float64, float64) (float64, error){'+': add, '-': substract, '/': divide, '*': multiply}
+var FUNCTION_MAP = map[string]operator{"+": add, "-": substract, "/": divide, "*": multiply}
 
 type op_result struct {
 	result     float64
@@ -36,12 +39,16 @@ type op_result struct {
 	error      error
 }
 
-type base_operation[IN any] func(operands []IN) op_result
+type operation interface {
+	execute_operation()
+	get_results() (float64, string, error)
+	set_wait_group(*sync.WaitGroup)
+}
 
 // simple operation
 type simple_operation struct {
 	operands   []float64
-	operator   base_operation[float64]
+	operator   string
 	wg         *sync.WaitGroup
 	result     float64
 	result_out string
@@ -49,7 +56,7 @@ type simple_operation struct {
 }
 
 func (s *simple_operation) execute_operation() {
-	result := s.operator(s.operands)
+	result := simple_operator(s.operands, s.operator)
 	if result.error == nil {
 		s.result_out = result.result_out
 		s.result = result.result
@@ -62,66 +69,42 @@ func (s *simple_operation) execute_operation() {
 	}
 }
 
+func (s *simple_operation) get_results() (float64, string, error) {
+	return s.result, s.result_out, s.error
+}
+
+func (s *simple_operation) set_wait_group(new_wg *sync.WaitGroup) {
+	s.wg = new_wg
+}
+
 // simple operator
-func simple_add(operands []float64) op_result {
+func simple_operator(operands []float64, operator string) op_result {
 	out := new(op_result)
 	out.result = operands[0]
 	out.result_out = fmt.Sprintf("(%.2f", operands[0])
+	var err error
 	for _, x := range operands[1:] {
-		out.result += x
-		out.result_out += fmt.Sprintf("+%.2f", x)
-	}
-	out.result_out += ")"
-	return *out
-}
-func simple_substract(operands []float64) op_result {
-	out := new(op_result)
-	out.result = operands[0]
-	out.result_out = fmt.Sprintf("(%.2f", operands[0])
-	for _, x := range operands[1:] {
-		out.result -= x
-		out.result_out += fmt.Sprintf("-%.2f", x)
-	}
-	out.result_out += ")"
-	return *out
-}
-func simple_multiply(operands []float64) op_result {
-	out := new(op_result)
-	out.result = operands[0]
-	out.result_out = fmt.Sprintf("(%.2f", operands[0])
-	for _, x := range operands[1:] {
-		out.result *= x
-		out.result_out += fmt.Sprintf("*%.2f", x)
-	}
-	out.result_out += ")"
-	return *out
-}
-func simple_divide(operands []float64) op_result {
-	out := new(op_result)
-	out.result = operands[0]
-	out.result_out = fmt.Sprintf("(%.2f", operands[0])
-	for _, x := range operands[1:] {
-		if x == 0 {
-			out.result = 0
-			tmp := "Error, can't divide by 0!\n{ "
+		out.result, err = FUNCTION_MAP[operator](out.result, x)
+		// this means division by zero
+		if err != nil {
+			tmp := fmt.Sprintf("%s\n(", err.Error())
 			for _, y := range operands[:len(operands)-1] {
-				tmp += fmt.Sprintf("%.2f, ", y)
+				tmp += fmt.Sprintf("%.2f %s", y, operator)
 			}
-			tmp += fmt.Sprintf("%.2f }", operands[len(operands)-1])
+			tmp += fmt.Sprintf(" %.2f)=0", operands[len(operands)-1])
 			out.error = errors.New(tmp)
 			return *out
 		}
-		out.result += x
-		out.result_out += fmt.Sprintf("/%.2f", x)
+		out.result_out += fmt.Sprintf(" %s %.2f", operator, x)
 	}
 	out.result_out += ")"
 	return *out
 }
 
-// composed operation
+// composed operation, the operation interface
 type composed_operation struct {
-	operands   []*simple_operation
-	operator   base_operation[*simple_operation]
+	operands   []operation
+	operator   string
 	wg         *sync.WaitGroup
 	result     float64
 	result_out string
@@ -129,7 +112,7 @@ type composed_operation struct {
 }
 
 func (c *composed_operation) execute_operation() {
-	result := c.operator(c.operands)
+	result := composed_operand(c.operands, c.operator)
 	if result.error == nil {
 		c.result = result.result
 		c.result_out = result.result_out
@@ -142,96 +125,59 @@ func (c *composed_operation) execute_operation() {
 	}
 }
 
+func (c *composed_operation) get_results() (float64, string, error) {
+	return c.result, c.result_out, c.error
+}
+
+func (c *composed_operation) set_wait_group(new_wg *sync.WaitGroup) {
+	c.wg = new_wg
+}
+
 // composed operator
-func composed_add(operands []*simple_operation) op_result {
+func composed_operand(operands []operation, operator string) op_result {
+	var curr_wg sync.WaitGroup
+	var err error
+	var tmp_out string
+	var tmp_res float64
 	out := new(op_result)
-	operands[0].execute_operation()
-	if operands[0].error != nil {
-		out.error = operands[0].error
+	curr_wg.Add(len(operands))
+	for _, x := range operands {
+		x.set_wait_group(&curr_wg)
+		go x.execute_operation()
+	}
+	curr_wg.Wait()
+	tmp_res, tmp_out, err = operands[0].get_results()
+	if err != nil {
+		out.error = err
 		return *out
 	}
-	out.result = operands[0].result
-	out.result_out = fmt.Sprintf("[%s", operands[0].result_out)
+	out.result = tmp_res
+	var starter, ender string
+	if strings.Contains(tmp_out, "[") || strings.Contains(tmp_out, "{") {
+		starter = "{"
+		ender = "}"
+	} else {
+		starter = "["
+		ender = "]"
+	}
+	out.result_out = fmt.Sprintf("%s%s", starter, tmp_out)
 	for _, x := range operands[1:] {
-		x.execute_operation()
-		if x.error != nil {
-			out.error = x.error
+		tmp_res, tmp_out, err = x.get_results()
+		if err != nil {
+			out.error = err
 			return *out
 		}
-		out.result += x.result
-		out.result_out += fmt.Sprintf("+%s", x.result_out)
-	}
-	out.result_out += "]"
-	return *out
-}
-func composed_substract(operands []*simple_operation) op_result {
-	out := new(op_result)
-	operands[0].execute_operation()
-	if operands[0].error != nil {
-		out.error = operands[0].error
-		return *out
-	}
-	out.result = operands[0].result
-	out.result_out = fmt.Sprintf("[%s", operands[0].result_out)
-	for _, x := range operands[1:] {
-		x.execute_operation()
-		if x.error != nil {
-			out.error = x.error
-			return *out
-		}
-		out.result -= x.result
-		out.result_out += fmt.Sprintf("-%s", x.result_out)
-	}
-	out.result_out += "]"
-	return *out
-}
-func composed_multiply(operands []*simple_operation) op_result {
-	out := new(op_result)
-	operands[0].execute_operation()
-	if operands[0].error != nil {
-		out.error = operands[0].error
-		return *out
-	}
-	out.result = operands[0].result
-	out.result_out = fmt.Sprintf("[%s", operands[0].result_out)
-	for _, x := range operands[1:] {
-		x.execute_operation()
-		if x.error != nil {
-			out.error = x.error
-			return *out
-		}
-		out.result *= x.result
-		out.result_out += fmt.Sprintf("*%s", x.result_out)
-	}
-	out.result_out += "]"
-	return *out
-}
-func composed_divide(operands []*simple_operation) op_result {
-	out := new(op_result)
-	operands[0].execute_operation()
-	if operands[0].error != nil {
-		out.error = operands[0].error
-		return *out
-	}
-	out.result = operands[0].result
-	out.result_out = fmt.Sprintf("[%s", operands[0].result_out)
-	for _, x := range operands[1:] {
-		x.execute_operation()
-		if x.error != nil {
-			out.error = x.error
-			return *out
-		} else if x.result == 0 {
-			out.result = 0
-			tmp := "Error, can't divide by 0!\n"
-			tmp += x.result_out
+		out.result, err = FUNCTION_MAP[operator](out.result, tmp_res)
+		if err != nil {
+			tmp := fmt.Sprintln(err.Error())
+			tmp += tmp_out
 			tmp += "=0"
 			out.error = errors.New(tmp)
 			return *out
 		}
-		out.result /= x.result
-		out.result_out += fmt.Sprintf("/%s", x.result_out)
+		out.result_out += fmt.Sprintf("%s%s", operator, tmp_out)
 	}
-	out.result_out += "]"
+	out.result_out += ender
 	return *out
 }
 
@@ -251,24 +197,26 @@ func handle_multiple_workers(multiCmd *flag.FlagSet, number_of_workers *int) {
 		os.Exit(WRONG_MULTI_CMD_ERROR)
 	}
 
-	operators := [4]base_operation[float64]{simple_add, simple_substract, simple_multiply, simple_divide}
+	operators := [4]string{"+", "-", "*", "/"}
 	tot_operators := len(operators)
 	operations := make([]simple_operation, *number_of_workers)
 
 	var goroutines_wg sync.WaitGroup
 	goroutines_wg.Add(*number_of_workers)
-	var curr_op base_operation[float64]
 	for i := 1; i <= *number_of_workers; i++ {
-		curr_op = operators[i%tot_operators]
-		operations[i-1] = simple_operation{[]float64{float64(i * i), float64(i) * rand.Float64()}, curr_op, &goroutines_wg, 0, "", nil}
+		operations[i-1] = simple_operation{[]float64{float64(i * i), float64(i) * rand.Float64()}, operators[i%tot_operators], &goroutines_wg, 0, "", nil}
 		go operations[i-1].execute_operation()
 	}
 	goroutines_wg.Wait()
+	var res float64
+	var out string
+	var err error
 	for i, op := range operations {
-		if op.error == nil {
-			fmt.Printf("Go-routine %d -> %s\n", i+1, op.result_out)
+		res, out, err = op.get_results()
+		if err == nil {
+			fmt.Printf("Go-routine %d -> %s=%.2f\n", i+1, out, res)
 		} else {
-			fmt.Printf("Go-routine %d Error -> %s\n", i+1, op.error)
+			fmt.Printf("Go-routine %d Error -> %s\n", i+1, err)
 		}
 	}
 }
@@ -286,31 +234,47 @@ func main() {
 	switch os.Args[1] {
 	case "multi":
 		handle_multiple_workers(multiCmd, number_of_workers)
-		break
+		return
 	case "simple":
 		fmt.Println("simple CMD")
 		operand1 := new(simple_operation)
-		operand1.operands = []float64{1, 3}
-		operand1.operator = simple_add
+		operand1.operands = []float64{1, 0}
+		operand1.operator = "+"
 
 		operand2 := new(simple_operation)
-		operand2.operands = []float64{1, 0}
-		operand2.operator = simple_multiply
+		operand2.operands = []float64{1, 2}
+		operand2.operator = "*"
+
+		comp_op1 := new(composed_operation)
+		comp_op1.operands = []operation{operand1, operand2}
+		comp_op1.operator = "+"
+
+		operand3 := new(simple_operation)
+		operand3.operands = []float64{1, 3}
+		operand3.operator = "+"
+
+		operand4 := new(simple_operation)
+		operand4.operands = []float64{1, 2}
+		operand4.operator = "*"
+
+		comp_op2 := new(composed_operation)
+		comp_op2.operands = []operation{operand3, operand4}
+		comp_op2.operator = "+"
 
 		comp_op := new(composed_operation)
-		comp_op.operands = []*simple_operation{operand1, operand2}
-		comp_op.operator = composed_divide
-
+		comp_op.operands = []operation{comp_op1, comp_op2}
+		comp_op.operator = "+"
 		comp_op.execute_operation()
-		if comp_op.error == nil {
-			fmt.Printf("Execution ended ok! %s\nWith result: %.2f", comp_op.result_out, comp_op.result)
+		result, out, err := comp_op.get_results()
+		if err == nil {
+			fmt.Printf("Execution ended ok! %s\nWith result: %.2f", out, result)
 		} else {
-			fmt.Println(comp_op.error)
+			fmt.Println(err)
 		}
-		break
+		return
 	case "from_file":
 		fmt.Println("from_file CMD")
-		break
+		return
 	default:
 		fmt.Println("You need to enter a basic comand:\n-multi\n-simple\n-from_file")
 		os.Exit(NO_BASE_CMD_ERROR)
